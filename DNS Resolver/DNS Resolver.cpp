2e2739db2a812d2532009f4abac42ebd1af4484e
Yatin Gupta <yatingupta7777@gmail.com>
2022-10-01 12:36:18 -0500

    Add initial FDH and QH code

--- DNS Resolver/DNS Resolver.cpp
+++ DNS Resolver/DNS Resolver.cpp
@@ -22,6 +22,40 @@ using namespace std;
 
 #define MAX_DNS_LEN 512
 
+/* flags */
+#define DNS_QUERY (0 << 15) /* 0 = query; 1 = response */
+#define DNS_RESPONSE (1 << 15)
+#define DNS_STDQUERY (0 << 11) /* opcode - 4 bits */
+#define DNS_AA (1 << 10) /* authoritative answer */
+#define DNS_TC (1 << 9) /* truncated */
+#define DNS_RD (1 << 8) /* recursion desired */
+#define DNS_RA (1 << 7) /* recursion available */
+
+#pragma pack(push,1) 
+class QueryHeader{
+public:
+    USHORT type;
+    USHORT c;
+};
+class FixedDNSheader {
+public:
+    USHORT ID;
+    USHORT flags;
+    USHORT questions;
+    USHORT auth;
+    USHORT answers;
+    USHORT additional;
+};
+class DNSanswerHdr {
+public:
+    u_short type;
+    u_short c;
+    u_int ttl;
+    u_short len;
+};
+#pragma pack(pop)
+
+
 int main(int argc, char** argv)
 {
     std::cout << "Hello World!\n";
@@ -32,15 +66,27 @@ int main(int argc, char** argv)
         printf("OR\n");
     }
 
-    char* lookup_host = argv[1];
-    char* dns_server_ip = argv[2];
+    string lookup_host = argv[1];
+    string dns_server_ip = argv[2];
 
     printf("%s\n", lookup_host);
     printf("%s\n", dns_server_ip);
 
-    DWORD IP = inet_addr(lookup_host);
+    int pkt_size = strlen(lookup_host.c_str()) + 2 + sizeof(FixedDNSheader) + sizeof(QueryHeader);
+    char* buf = new char[pkt_size];
+
+    FixedDNSheader* fdh = (FixedDNSheader*)buf;
+    QueryHeader* qh = (QueryHeader*)(buf + pkt_size - sizeof(QueryHeader));
+
+    fdh->flags = htons(DNS_QUERY | DNS_RD | DNS_STDQUERY);
+
+    DWORD IP = inet_addr(lookup_host.c_str());
+    qh->type=htons(DNS_PTR);
     if (IP == INADDR_NONE)
     {
-        printf("NO IP YO");
+        qh->type = htons(DNS_A);
     }
+    else {
+    }
+    qh->c = htons(DNS_INET);
 }
